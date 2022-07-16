6c3993ae5a2ce22399ef9d6f53fd97c3e9334455
caveman418 <104240436+caveman418@users.noreply.github.com>
2022-07-16 12:44:00 -0500

    Initial javascript
    Created gameBoard module that stores the current board and has a few methods.

--- script.js
+++ script.js
@@ -2,13 +2,41 @@
     const newGameButton = document.querySelector('.new-game-button');
     const startGamebutton = document.querySelector('.start-game');
     const popupMenu = document.querySelector('.popup');
+    const squares = document.querySelectorAll('.square');
 
-    newGameButton.addEventListener('click', () => {
+    newGameButton.addEventListener('mousedown', () => {
         popupMenu.classList.add('active');
     });
 
-    startGamebutton.addEventListener('click', () => {
+    startGamebutton.addEventListener('mousedown', () => {
         popupMenu.classList.remove('active');
+    });
+
+    squares.forEach((square) => {
+        square.addEventListener('mousedown', () => {
+            let player = (gameBoard.counter() % 2 === 0) ? 'x':'o';
+            gameBoard.add(square.id.slice(-1),player);
+            Display.update();
+        })
     })
 })();
 
+const gameBoard = (function() {
+    let count = 0;
+    let board = (new Array(9)).fill('');
+    const counter = () => {
+        return count;
+    }
+    const current = () => {
+        return board;
+    }
+    const add = (pos,player) => {
+        count++;
+        board[pos] = player;
+    }
+    const clear = () => {
+        count = 0;
+        board.fill('');
+    }
+    return {counter, current, add, clear};
+})();
