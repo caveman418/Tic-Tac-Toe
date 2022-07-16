4e9a9fa9a6c5b4aaaa07f733bebb52eaf02d505f
caveman418 <104240436+caveman418@users.noreply.github.com>
2022-07-16 15:24:06 -0500

    Made board usable
    Now clicking in a square puts an 'x' or 'o' and you can't put a mark where one already exists.

--- script.js
+++ script.js
@@ -13,30 +13,34 @@
     });
 
     squares.forEach((square) => {
+        let pos = square.id.slice(-1);
         square.addEventListener('mousedown', () => {
             let player = (gameBoard.counter() % 2 === 0) ? 'x':'o';
-            gameBoard.add(square.id.slice(-1),player);
-            Display.update();
-        })
-    })
+            gameBoard.add(pos,player);
+            square.textContent = gameBoard.searchIndex(pos);
+        });
+    });
 })();
 
 const gameBoard = (function() {
     let count = 0;
     let board = (new Array(9)).fill('');
+    
     const counter = () => {
         return count;
     }
-    const current = () => {
-        return board;
+    const searchIndex = (index) => {
+        return board[index];
     }
     const add = (pos,player) => {
-        count++;
-        board[pos] = player;
+        if (board[pos]==='') {
+            count++;
+            board[pos] = player;
+        }
     }
     const clear = () => {
         count = 0;
         board.fill('');
     }
-    return {counter, current, add, clear};
+    return {counter, searchIndex, add, clear};
 })();
